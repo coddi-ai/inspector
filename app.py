4fbabe3c334d426c45947c4afa4346b61891b441
Patricio Ortiz <132593508+portizv26@users.noreply.github.com>
2024-07-26 12:33:15 -0400

    connection with azure cloud
    process audio replaced to be consumed on cloud instance

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-from utils import process_audio
+from utils import *
 from PIL import Image
-import os
 import time
 import subprocess
+from dotenv import load_dotenv
+import os
 
-# Create a base directory to save the uploaded files
-base_upload_dir = "uploads"
-if not os.path.exists(base_upload_dir):
-    os.makedirs(base_upload_dir)
-    
+load_dotenv()
+
 # Sidebar for machine and component information
 st.sidebar.header("Machine and Component Information")
 machine = st.sidebar.text_input("Machine")
@@ -21,47 +19,42 @@ st.title("Image and Audio Upload MVP")
 # Capture image using camera
 st.header("Capture an Image")
 uploaded_image = st.camera_input("Take a picture")
-image_path = None
-audio_path = None
+image_buffer = None
+audio_buffer = None
 
+# Image upload
 if uploaded_image is not None:
-    # Generate a unique directory name based on the current time
-    upload_time = int(time.time())
-    upload_dir = os.path.join(base_upload_dir, f"upload_{upload_time}")
-    os.makedirs(upload_dir, exist_ok=True)
-
-    # Save the captured image
-    image_path = os.path.join(upload_dir, "image.jpg")
-    with open(image_path, "wb") as f:
-        f.write(uploaded_image.getbuffer())
+    image_buffer = bytes(uploaded_image.getbuffer())
     # Display the captured image
     image = Image.open(uploaded_image)
     st.image(image, caption="Captured Image", use_column_width=True)
 
 # Audio upload
 st.header("Upload an Audio File")
-uploaded_audio = st.file_uploader("Record an audio...", type=["mp3", "wav", "ogg"])
+uploaded_audio = st.file_uploader("Record an audio...", type=["mp3", "wav"])
 if uploaded_audio is not None:
-    if image_path is not None:
-        # Save the uploaded audio in the same directory as the image
-        audio_path = os.path.join(upload_dir, uploaded_audio.name)
-        with open(audio_path, "wb") as f:
-            f.write(uploaded_audio.getbuffer())
-        
-        # Convert audio to mp3 if it is not in mp3 format
-        if not audio_path.endswith(".mp3"):
-            converted_audio_path = os.path.join(upload_dir, "audio.mp3")
-            subprocess.run(["ffmpeg", "-i", audio_path, converted_audio_path])
-            audio_path = converted_audio_path
-
-        # Display the uploaded audio file
-        st.audio(audio_path, format='audio/mpeg')
+    audio_buffer = bytes(uploaded_audio.getbuffer())
+    # Display the uploaded audio file
+    st.audio(uploaded_audio, format='audio/mpeg')
 
 # Process button
-if uploaded_image is not None and uploaded_audio is not None and machine and component:
+if uploaded_image is not None and uploaded_audio is not None:
     if st.button("Process"):
-        st.write("Processing audio...")
-        processed_audio_result = process_audio(audio_path)
-        st.write(f"{processed_audio_result}")
+        print("Uploading files to Azure Storage...")
+        upload_time = int(time.time())
+        image_blob_name = f"upload_{upload_time}/image.jpg"
+        audio_blob_name = f"upload_{upload_time}/{uploaded_audio.name}"
+
+        print("Uploading images to Azure Storage...")
+        # Upload both files to Azure Storage
+        image_url, audio_url = upload_files_to_azure(image_buffer, audio_buffer, image_blob_name, audio_blob_name)
+
+        print(f"Urls of the uploaded files: \n{image_url} \n{audio_url}")
+
+        processed_audio_result = process_audio(audio_blob_name)
+        if processed_audio_result[1] == 0:
+            st.write(f"Error: {processed_audio_result[0]}")
+        else:
+            st.write(f"{processed_audio_result[0]}")
 else:
     st.write("Please fill in all the information and upload both files to proceed.")
--- utils.py
+++ utils.py
@@ -1,55 +1,105 @@
 import streamlit as st
 import requests
 import os
-from openai import OpenAI
-from io import BytesIO
-import logging
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
 
-def get_client():
+load_dotenv()
+
+def get_function_url():
     try:
-        api_key = st.secrets["secret_key"]
-    except:
-        api_key = os.getenv("OPENAI_API_KEY")
-       
-    if not api_key:
-        raise ValueError("No OpenAI API key found in environment variables")
-    client = OpenAI(api_key=api_key)
-    return client
+        if ".env" in os.listdir():
+            load_dotenv()
+            function_url = os.getenv("FUNCTION_URL_AZURE")
+            subscription_key = os.getenv("SUBSCRIPTION_KEY")
+        else:
+            function_url = st.secrets["FUNCTION_URL_AZURE"]
+            subscription_key = st.secrets["SUBSCRIPTION_KEY"]
 
-def transcribe_audio(client, audio_file):
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        language='es',
-        file=audio_file,
-    )
-    return transcription.text
+        if not function_url or not subscription_key:
+            raise ValueError("Function URL or Subscription Key not found in environment variables")
 
-def refine_transcription(client, transcription, temperature=0.8):
-    system_prompt = """
-Eres un asistente que extrae la información relevante para explicar una inspección visual. 
-Transforma el texto entregado, para que explique de manera clara y concisa lo observado en la inspección, centrandose en los hechos y observaciones generadas.
-El contexto de los reportes es en contexto minero, haciendo inspecciones manuales sobre equipos, por lo cual se espera que se utilicen conceptos técnicos de minería y mecanica.
-El texto generado debe tener el siguiente formato:
-Fecha (si es que está disponible)
-* Observaciones (en bullet points).
+        return function_url, subscription_key
 
-Errores comunes:
-palaciete = Pala 7
-"""
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        temperature=temperature,
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": transcription}
-        ]
-    )
-    return response.choices[0].message.content
+    except Exception as e:
+        raise ValueError(f"No function URL or Subscription Key found in environment variables: {e}")
 
-def process_audio(audio_path):
-    client = get_client()
-    audio_file = open(audio_path, "rb")
-    transcription = transcribe_audio(client, audio_file)
-    transcription = refine_transcription(client, transcription)
+def get_azure_credentials():
+    try:
+        if ".env" in os.listdir():
+            load_dotenv()
+            connect_str = os.getenv("AZURE_CONNECTION_STRING")
+            container_name = os.getenv("AZURE_CONTAINER_NAME")
+        else:
+            connect_str = st.secrets["AZURE_CONNECTION_STRING"]
+            container_name = st.secrets["AZURE_CONTAINER_NAME"]
 
-    return transcription
+        if not connect_str or not container_name:
+            raise ValueError("Azure Storage connection string or container name not found in environment variables")
+
+        return connect_str, container_name
+
+    except Exception as e:
+        raise ValueError(f"No Azure Storage connection string or container name found in environment variables: {e}")
+
+def upload_to_azure(file_buffer, file_name, connect_str, container_name):
+    """
+    Uploads a file to Azure Blob Storage.
+    
+    Args:
+    - file_buffer: The file buffer to upload.
+    - file_name: The name of the file in Azure Blob Storage.
+    - container_name: The name of the Azure container.
+    - connect_str: The connection string for Azure Blob Storage.
+    
+    Returns:
+    - file_url: The URL of the uploaded file.
+    """
+    # Initialize Azure Storage Blob client
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    container_client = blob_service_client.get_container_client(container_name)
+    # Get the blob client
+    blob_client = container_client.get_blob_client(file_name)
+    # Upload the file
+    blob_client.upload_blob(file_buffer, overwrite=True)
+    
+    # Get the URL of the uploaded file
+    file_url = blob_client.url
+    return file_url
+
+def upload_files_to_azure(image_buffer, audio_buffer, image_name, audio_name):
+    """
+    Uploads both image and audio files to Azure Blob Storage.
+    
+    Args:
+    - image_buffer: The image file buffer to upload.
+    - audio_buffer: The audio file buffer to upload.
+    - image_name: The name of the image file in Azure Blob Storage.
+    - audio_name: The name of the audio file in Azure Blob Storage.
+    - container_name: The name of the Azure container.
+    - connect_str: The connection string for Azure Blob Storage.
+    
+    Returns:
+    - image_url: The URL of the uploaded image file.
+    - audio_url: The URL of the uploaded audio file.
+    """
+    container_name, connect_str = get_azure_credentials()
+    image_url = upload_to_azure(image_buffer, image_name, container_name, connect_str)
+    audio_url = upload_to_azure(audio_buffer, audio_name, container_name, connect_str)
+    return image_url, audio_url
+
+def process_audio(audio_file_name):
+    function_url, subscription_key = get_function_url()
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+
+    payload = {"file_name": audio_file_name}
+
+    print("Sending request to the function...")
+    print(function_url,'\n',subscription_key,'\n',payload,'\n',headers)
+
+    response = requests.post(function_url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        return (response.text, 1)
+    else:
+        return (f"Error: {response.status_code} - {response.text}", 0)
